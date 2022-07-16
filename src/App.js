417bcdd3b74e5b8cc0f1b88bf6debf90783c5ab9
sehaj anand <sehajanand.321@gmail.com>
2022-07-16 11:29:06 +0530

    added header footer and main sections

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
    <>
+   <header>
+    <Navbar/>
+   </header>
+   <main></main>
+   <footer>&copy; Copyright NIT Srinagar </footer>
    </>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <>
+    <nav ><p id='scroll'>Smart Irrigation System</p></nav>
+    </>
+  )
+}
